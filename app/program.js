2787859e348ec39c4c2dd56721df7a775d8e731f
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-15 21:23:54 +0200

    Add file caclTime

--- app/program.js
+++ app/program.js
@@ -1,5 +1,7 @@
 process.stdin.setEncoding('utf-8');
-var OSinfo = require('../modules/OSinfo');
+var OSinfo = require('../modules/OSInfo');
+
+
 process.stdin.on('readable', function(){
     var input = process.stdin.read();
     if(input !== null){
