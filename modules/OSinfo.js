80285c0658a3644b1cf7e9eacc5c229c407a6247
Daniel Minschinski <danielminschinski@gmail.com>
2019-10-15 21:24:12 +0200

    Add file caclTime

--- modules/OSinfo.js
+++ modules/OSinfo.js
@@ -1,4 +1,6 @@
 var os = require('os');
+var calcTime = require('../modules/calcTime');
+
 function getOSinfo(){
     var type = os.type();
     if(type === 'Darwin'){
@@ -8,13 +10,13 @@ function getOSinfo(){
     }
     var release = os.release();
     var cpu = os.cpus()[0].model;
-    var uptime = os.uptime();
+    var uptime = calcTime.print();
     var userInfo = os.userInfo();
     console.log('System:', type);
     console.log('Release:', release);
     console.log('CPU model:', cpu);
-    console.log('Uptime: ~', (uptime / 60).toFixed(0), 'min');
+    console.log('Uptime:', uptime);
     console.log('User name:', userInfo.username);
     console.log('Home dir:', userInfo.homedir);
 }
-export.print = getOSinfo;
+exports.print = getOSinfo;
